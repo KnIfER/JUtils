67272b456832408018ad668cf48ec9a225f945f9
knziha <1445952621@qq.com>
2019-07-26 13:05:18 +0800

    Update vectorpathTweaker.java

--- shape/vectorpathTweaker.java
+++ shape/vectorpathTweaker.java
@@ -10,34 +10,75 @@ public class vectorpathTweaker {
 	public static void main(String[] args) {
 		//改造vector path
 		float viewportHeight=24, viewportWidth=24;
-		float scaler = 0.75f;
+		float scaler = .5f;
 		float scalerY = scaler;
 		float transX=0f;
 		float transY=0f;
 		boolean tranverse = false;
 		boolean flipX = false;
+		boolean keep_rel_group = true;
 		//String pathdata = "M20,19v-4L8,7 12,4.5 20,9V5L12,0.5 4,5v4 l12.63,7.89L12,19.5 4,15v4l8,4.5z";
 		//pathdata = "M20,19v-4L8,7 12,4.5 20,9V5L12,0.5 4,5v4 L16.63,16.98 12,19.5 4,15v4L12,23.5z";
-		String pathdata = "M0,12L0,24 24,24 24,12 0,12z";
+		String pathdata = "M12,4c-4.97,0 -9,4.03 -9,9s4.02,9 9,9c4.97,0 9,-4.03 9,-9s-4.03,-9 -9,-9z";
 		StringBuilder pathbuilder = new StringBuilder();
 		Pattern reg = Pattern.compile("M|l|L|z|s|c|V|v|h|H| ");
+		Pattern regLower = Pattern.compile("[a-z]");
 		Matcher m = reg.matcher(pathdata);
 		int idx=0;
-		String lastCommand;
+		String lastCommand = null;
+		Float[] firstOrg=null;
+		Float[] Org=null;
+		float[] deltaOrg=new float[2];
 		while(m.find()) {
 			int now =m.start();
 			if(idx!=-1 && now>idx) {
-				//CMN.Log(pathdata.substring(idx+1,now));
-				pathbuilder.append(lastCommand = pathdata.substring(idx,idx+1));
+				String command = pathdata.substring(idx,idx+1);
+				if(!command.equals(" "))
+					lastCommand=command;
+				boolean xiaoxie = regLower.matcher(lastCommand).matches();
+				boolean isOrg = lastCommand.equals("M");
+				boolean isfirstOrg = isOrg?firstOrg==null:false;
+				if(isfirstOrg)CMN.Log("org#1="+pathdata.substring(idx+1,now));
+				CMN.Log("command: "+command+" "+lastCommand+" "+xiaoxie);
+				CMN.Log(pathdata.substring(idx+1,now));
+				pathbuilder.append(pathdata.substring(idx,idx+1));
 				String[] arr = pathdata.substring(idx+1,now).split(",");
 				if(arr.length==2) {
-					float x=Float.valueOf(tranverse?arr[1]:arr[0]);
-					x=(1-scaler)*viewportWidth/2 + x*scaler + transX;
-					if(flipX) x = viewportWidth-x;
+					float x=Float.valueOf(arr[0]);
+					if(isOrg) {
+						Org=new Float[2];
+						Org[0]=x;
+						if(isfirstOrg) {
+							firstOrg=Org;
+							deltaOrg[0]=transX;
+						}else if(keep_rel_group) {
+							deltaOrg[0]=scaler*(x-firstOrg[0])+firstOrg[0]-x+transX;
+						}
+					}else if(xiaoxie){
+						x=x*scaler;
+						if(flipX) x = -x;
+					}else {
+						x=scaler*(x-Org[0])+Org[0];
+						if(flipX) x = viewportWidth-x;
+					}
+					if(!xiaoxie)x+=deltaOrg[0];
 					pathbuilder.append(trimFloatString(String.format("%.2f", x)));
 					pathbuilder.append(",");
-					x=Float.valueOf(tranverse?arr[0]:arr[1]);
-					x=(1-scalerY)*viewportHeight/2 + x*scalerY + transY;
+					x=Float.valueOf(arr[1]);
+					if(isOrg) {
+						Org[1]=x;
+						if(isfirstOrg) {
+							deltaOrg[1]=transY;
+						}else if(keep_rel_group) {
+							deltaOrg[1]=scalerY*(x-firstOrg[1])+firstOrg[1]-x+transY;
+						}
+					}else if(xiaoxie){
+						x=x*scalerY;
+						if(flipX) x = -x;
+					}else {
+						x=scalerY*(x-Org[1])+Org[1];
+					}
+					if(!xiaoxie)x+=deltaOrg[1];
 					pathbuilder.append(trimFloatString(String.format("%.2f", x)));
 				}
 				else {
