c5149d622d046fd12bb8a5c14b5587b1ed562d8d
knziha <1445952621@qq.com>
2019-08-15 21:53:39 +0800

    Update shivo3.0.py

--- ffmpeg/fftools/shivo3.0.py
+++ ffmpeg/fftools/shivo3.0.py
@@ -15,8 +15,8 @@ import codecs
 
 
 #ffmpegexe = r'D:\Downloads\ffmpeg-4.1.3-win64-shared\bin\ffmpeg.exe'
-ffmpegexe = r'ffmpeg.exe'
-    
+#ffmpegexe = r'ffmpeg.exe'
+ffmpegexe = r'D:\Downloads\ffmpeg-4.2-win64-static\bin\ffmpeg.exe'
     
 while True:
     f = codecs.open(flibf,"r","utf-16")
@@ -28,7 +28,7 @@ while True:
     bml = flib[bmlOff:bmlOff2]
     #print bml
     o=bml.find(str(theta)+"=")
-    findCount = -1#这是可能要处理的文件个数
+    findCount = 0
     while(bml.find("\n",o)!=-1):
         o=bml.find("\n",o)+2
         findCount+=1
@@ -42,10 +42,13 @@ while True:
         videoPathNames.append(videoPathName)
         videoPathName = str(input("videoPathName"))
         
-	
+    accurate_recode=False
     for videoPathI in videoPathNames:
-        for fileIdx in range(1,findCount+1):
-            ducOff = bml.find(str(1+theta+fileIdx-1)+"=")
+        if videoPathI[-1]==":":
+            accurate_recode=True
+            videoPathI=videoPathI[0:-1]
+        for fileIdx in range(0,findCount):
+            ducOff = bml.find(str(0+theta+fileIdx)+"=")
             ducOff2 = bml.find("\r",ducOff)
             try:
                 videoPathName = bml[ducOff:ducOff2].split("=")[1]
@@ -59,8 +62,7 @@ while True:
                 o=videoPathName.find("\\",o)+1
             videoName = videoPathName[o:]
             videoPath = videoPathName[:o]
-            #获取文件名END
-            print("videoName",videoName,videoPath)
+            #print("videoName is : ",videoName,videoPath, "\n")
             #获取当前文件的书签列表
             #print flib.find("[BMItem_"+str(theta+fileIdx))
             itemSectionOff = flib.find("[BMItem_"+str(theta+fileIdx))
@@ -102,14 +104,40 @@ while True:
                     new_path = videoPath+videoName[:-4]+"_SchivoNo."+str(cc)+".mp4"
                     my_file = Path(new_path)
                 cc+=1
-                mingling=(tm1)+"  -t  "+str(1.0*(end-start)/1000)+" -accurate_seek "   +" -i "+'"'+videoPathName+'"'+   " -codec copy -avoid_negative_ts 1 -strict -2 "   +'"'+new_path+'"'
-                print ("\r\ncommand on queue: FFMPEG "+mingling)
-                mingling = ffmpegexe + r' -ss '+ mingling
+                new_path=' -ss "'+new_path+'"'
+                inputfile="  "+"-i "+'"'+videoPathName+'"'
+                sst=r' -ss '+ (tm1)+"  -t  "+str(1.0*(end-start)/1000)
+                codec=" -codec copy -avoid_negative_ts 1 -strict -2 "
+                if accurate_recode:
+                    codec=" -codec copy  -c:v libx264 -avoid_negative_ts 1 -strict -2 "
+                offset_and_input=sst+inputfile
+                if accurate_recode:
+                    offset_and_input=inputfile+sst
+                    
+                codec=" -codec copy -avoid_negative_ts 1 -strict -2 "
+                offset_and_input=inputfile+sst
+                    
+                mingling=offset_and_input+codec+new_path
+                print ("\r\ncommand on queue: FFMPEG "+mingling+"\n")
+                mingling = ffmpegexe + mingling
                 
                 os.system(mingling+" -n")
-
+            break
+            
         #os.system("pause")
 
+#   -c:v libx264
+
+#倒放视频:
+#ffmpeg.exe -i INPUT -vf reverse -af areverse -preset superfast out_reversed.mp4
+#1.视频倒放，无音频
+#ffmpeg.exe -i inputfile.mp4 -filter_complex [0:v]reverse[v] -map [v] -preset superfast reversed.mp4
+# 
+#2.视频倒放，音频不变
+#ffmpeg.exe -i inputfile.mp4 -vf reverse reversed.mp4
+# 
+#3.音频倒放，视频不变
+#ffmpeg.exe -i inputfile.mp4 -map 0 -c:v copy -af "areverse" reversed_audio.mp4
 
 
 
